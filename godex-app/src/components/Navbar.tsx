1a899e24bf8930d9262e19494a1e26a33847882f
ve <ve@datora.de>
2024-08-11 19:58:37 +0200

    navbar styles

--- godex-app/src/components/Navbar.tsx
+++ godex-app/src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import { Link, useNavigate } from "react-router-dom";
 import cetoddle1 from "../img/cetoddle1.png";
+import { useSelectedPokemon } from "./context/SelectedPokemonContext";
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
+  const { selectedPokemon } = useSelectedPokemon();
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -10,11 +12,21 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav>
+    <nav className="navbar">
+      <div className="navlinks-left">
+        <img
+          src={cetoddle1}
+          alt="Logo"
+          style={{ height: "50px", marginRight: "1rem" }}
+        />
+        <Link to="/">Home</Link>
+        <Link to="/add-pokemon">Add Pokemon</Link>
+        <Link to="/user-details">User details</Link>
+        <button onClick={handleLogout}>
+          Show selected ({selectedPokemon.length})
+        </button>
+      </div>
       <button onClick={handleLogout}>Logout</button>
-      <Link to="/">Home</Link>
-      <Link to="/add-pokemon">Add Pokemon</Link>
-      <Link to="/user-details">User details</Link>
     </nav>
   );
 };
